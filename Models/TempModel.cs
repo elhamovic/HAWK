65f9276259d7688b4b48e886acb33c8106842921
elhamovic <elhamss17@yahoo.com>
2022-03-23 09:54:08 +0300

    Temp Model

---
+++ Models/TempModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HAWK_v.Models
+{
+    public class TempModel
+    {
+       
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Permission Start Date:")]
+        public string PStartDate { get; set; }
+        [DisplayName("Permission End Date:")]
+        public string PEndDate { get; set; }
+
+    }
+}
